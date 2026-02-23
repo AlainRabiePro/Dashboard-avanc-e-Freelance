f8f2684096ac1f4df3bc4b80202247e2d42d4d27
AlainR <rabie.alain001@gmail.com>
2026-02-23 17:27:15 +0100

    Ok

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4326606302-6b292",
-  "appId": "1:1068778746636:web:59f391a6568ed980cb4f29",
-  "apiKey": "AIzaSyC0r12z7rxuXQKVnPBM_YnXRWyKY35VsPs",
-  "authDomain": "studio-4326606302-6b292.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1068778746636"
+  projectId: "studio-4326606302-6b292",
+  appId: "1:1068778746636:web:59f391a6568ed980cb4f29",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: "studio-4326606302-6b292.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1068778746636"
 };
